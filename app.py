052e8cb9880f114b5daaae7f60af929dfe2dba53
Karen <karen.m.mendoza0102@gmail.com>
2025-10-09 00:26:21 -0300

    projeto finalizado

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 # O h1 define que o markdown representa o Titulo(cabeçalho)
-st.markdown("<h1 style='color: #ff9bb8;text-align: center;'>Análise exploratória de dados sobre vendas de carros:</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color: #ff9bb8;text-align: center;'>Análise exploratória de dados sobre anúncios de carros:</h1>", unsafe_allow_html=True)
 
 #lendo o dataframe
 car_data = pd.read_csv('vehicles.csv') 
@@ -13,7 +13,7 @@ st.dataframe(car_data)
 
 # p = significa um texto simples, com ajuda do html mudei a cor.
 # unsafe_allow_html=True, esse argumento permite usar HTML diretamente na aplicação.
-st.markdown("<p style='color: #fed7e2;'>Criando um histograma para o conjunto de dados de anúncios de vendas de carros:</p>", unsafe_allow_html=True)  
+st.markdown("<p style='color: #fed7e2;'>Aperte o botão para criar um histograma baseado nos valores da quilometragem dos carros:</p>", unsafe_allow_html=True)  
 
 # usando HTML para definir a cor de todos os botões.
 st.markdown("""
@@ -32,7 +32,7 @@ if hist_button:
     # .plotly_chart, serve para exibir o gráfico na aplicação.
     st.plotly_chart(fig, use_container_width=True)
 
-st.markdown("<p style='color:#fed7e2;'>Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros:</p>", unsafe_allow_html=True) 
+st.markdown("<p style='color:#fed7e2;'>Aperte o botão para criar um gráfico de dispersão da relação entre a quilometragem e o preço dos carros:</p>", unsafe_allow_html=True) 
 
 #Criando um botão e um gráfico de dispersão.
 dispersao_button = st.button('Criar Gráfico de Dispersão')
@@ -41,15 +41,15 @@ if dispersao_button:
     st.plotly_chart(figs, use_container_width=True)
 
 #Criando um gráfico pizza.
-fig4 = px.pie(car_data, names='fuel', title='Distribuição dos tipos de combustível',color_discrete_sequence=['#FF69B4', '#00BFFF', 'red','#FFA500','#9D00FF'  ])
+fig4 = px.pie(car_data, names='fuel', title='Distribuição dos tipos de combustível:',color_discrete_sequence=['#FF69B4', '#00BFFF', 'red','#FFA500','#9D00FF'  ])
 st.plotly_chart(fig4)
 
 #agrupamento e criação da um grafico de barras para saber a médias dos preços baseado nas condições do carro.
 receita = car_data.groupby('condition')['price'].mean().reset_index()
-fig3 = px.bar(receita, x = 'condition', y='price', title='Receita média baseada na condição dos carros:',color_discrete_sequence=['pink'])
+fig3 = px.bar(receita, x = 'condition', y='price', title='Preço médio baseado na condição dos carros:',color_discrete_sequence=['pink'])
 st.plotly_chart(fig3)
 
 #agrupamento e criação da um grafico de barras para saber a médias dos preços baseado nos modelos de carros.
 model_price = car_data.groupby('model', as_index=False)['price'].mean()
-fig5 = px.bar(model_price, x='model', y='price', title='Preço médio por modelo',color_discrete_sequence=['pink'])
+fig5 = px.bar(model_price, x='model', y='price', title='Preço médio baseado nos modelos dos carros:',color_discrete_sequence=['pink'])
 st.plotly_chart(fig5)
