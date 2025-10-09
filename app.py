f8a52bab3a16b02dd84335a7b05045b2c0de5af1
Karen <karen.m.mendoza0102@gmail.com>
2025-10-09 00:13:03 -0300

    finalizando o projeto

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
-st.header('Análise exploratória de dados sobre vendas de carros.')
 
-car_data = pd.read_csv('vehicles.csv') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
-        
-if hist_button: # se o botão for clicado
-            # escrever uma mensagem
-    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-            
-            # criar um histograma
+# O h1 define que o markdown representa o Titulo(cabeçalho)
+st.markdown("<h1 style='color: #ff9bb8;text-align: center;'>Análise exploratória de dados sobre vendas de carros:</h1>", unsafe_allow_html=True)
+
+#lendo o dataframe
+car_data = pd.read_csv('vehicles.csv') 
+
+#exibindo o dataframe na aplicação web, usando o streamlit.
+st.dataframe(car_data) 
+
+# p = significa um texto simples, com ajuda do html mudei a cor.
+# unsafe_allow_html=True, esse argumento permite usar HTML diretamente na aplicação.
+st.markdown("<p style='color: #fed7e2;'>Criando um histograma para o conjunto de dados de anúncios de vendas de carros:</p>", unsafe_allow_html=True)  
+
+# usando HTML para definir a cor de todos os botões.
+st.markdown("""
+    <style>
+    .stButton>button {
+        background-color:#ff9bb8;  
+        color: white;               
+        height: 50px;  }
+    </style>
+""", unsafe_allow_html=True)
+
+#Criando um botão e um histograma.
+hist_button = st.button('Criar histograma')
+if hist_button:     
     fig = px.histogram(car_data, x="odometer")
-        
-            # exibir um gráfico Plotly interativo
+    # .plotly_chart, serve para exibir o gráfico na aplicação.
     st.plotly_chart(fig, use_container_width=True)
 
+st.markdown("<p style='color:#fed7e2;'>Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros:</p>", unsafe_allow_html=True) 
 
+#Criando um botão e um gráfico de dispersão.
 dispersao_button = st.button('Criar Gráfico de Dispersão')
 if dispersao_button:
-    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-    figs = px.scatter(car_data, x="odometer", y="price") 
+    figs = px.scatter(car_data, x="odometer", y="price",color_discrete_sequence=['pink']) 
     st.plotly_chart(figs, use_container_width=True)
+
+#Criando um gráfico pizza.
+fig4 = px.pie(car_data, names='fuel', title='Distribuição dos tipos de combustível',color_discrete_sequence=['#FF69B4', '#00BFFF', 'red','#FFA500','#9D00FF'  ])
+st.plotly_chart(fig4)
+
+#agrupamento e criação da um grafico de barras para saber a médias dos preços baseado nas condições do carro.
+receita = car_data.groupby('condition')['price'].mean().reset_index()
+fig3 = px.bar(receita, x = 'condition', y='price', title='Receita média baseada na condição dos carros:',color_discrete_sequence=['pink'])
+st.plotly_chart(fig3)
+
+#agrupamento e criação da um grafico de barras para saber a médias dos preços baseado nos modelos de carros.
+model_price = car_data.groupby('model', as_index=False)['price'].mean()
+fig5 = px.bar(model_price, x='model', y='price', title='Preço médio por modelo',color_discrete_sequence=['pink'])
+st.plotly_chart(fig5)
